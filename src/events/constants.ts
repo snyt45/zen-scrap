58c37ce0bf9d7e18e6f6f7b117f4092b4517c7d4
yuta.sano <snyt45@gmail.com>
2026-03-11 00:04:36 +0900

    EventBusとイベント定数を追加
    Created by author with Claude Code

---
+++ src/events/constants.ts
@@ -0,0 +1,6 @@
+export const EVENTS = {
+  SCRAP_SELECT: "scrap:select",
+  SCRAP_CREATE_REQUEST: "scrap:create-request",
+  SCRAP_CHANGED: "scrap:changed",
+  NAV_BACK_TO_LIST: "nav:back-to-list",
+} as const;
